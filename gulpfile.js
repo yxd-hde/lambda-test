7873e3208d8fdc7419c7f84c5630a0af798c9cbf
YANG Xudong <xudong.yang@hde.co.jp>
2015-10-08 15:33:36 +0900

    Add package setting.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp-param')(require('gulp'), process.argv);
+var zip = require('gulp-zip');
 var run = require('gulp-run');
+var del = require('del');
+var install = require('gulp-install');
+var runSequence = require('run-sequence');
+var awsLambda = require("node-aws-lambda");
 
 gulp.task('default', function() {
   console.log('test');
@@ -7,6 +12,37 @@ gulp.task('default', function() {
 
 gulp.task('lambda', function(event) {
   run('node_modules/lambda-local/bin/lambda-local -l index.js -e ' + event)
-    .exec()
-    .pipe(gulp.dest('output'));
+    .exec();
+});
+
+gulp.task('clean', function() {
+  return del(['./dist', './dist.zip']);
+});
+
+gulp.task('js', function() {
+  return gulp.src(['index.js', 'lib/**/*'], {
+    base: '.'
+  }).pipe(gulp.dest('dist/'));
+});
+
+gulp.task('node-mods', function() {
+  return gulp.src('./package.json')
+    .pipe(gulp.dest('dist/'))
+    .pipe(install({
+      production: true
+    }));
+});
+
+gulp.task('zip', function() {
+  return gulp.src(['dist/**/*', '!dist/package.json'])
+    .pipe(zip('dist.zip'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('package', function() {
+  return runSequence(
+    ['clean'],
+    ['js', 'node-mods'],
+    ['zip']
+  );
 });
